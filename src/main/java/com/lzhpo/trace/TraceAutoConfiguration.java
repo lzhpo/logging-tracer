50ab03df5c2a8ebca13fc01823f1626fc3c03cba
lzhpo <lzhpo1024@gmail.com>
2022-06-14 19:30:45 +0800

    First commit

---
+++ src/main/java/com/lzhpo/trace/TraceAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.lzhpo.trace;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Trace auto configure
+ *
+ * @author lzhpo
+ */
+@Configuration
+public class TraceAutoConfiguration {
+
+}
